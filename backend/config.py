ef5ba88d70792e8fb17a4f80d5bcf880ca8f04b7
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-06-01 14:08:53 -0700

    Add comments to the config.py

--- backend/config.py
+++ backend/config.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv
 loaded = load_dotenv()
 
 class Config:
+    # Get the DATABASE_URL from .env
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+    # stop tracking every changes in objects
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Get the SECRET_KEY from .env
     SECRET_KEY = os.getenv('SECRET_KEY')
