e3c79370db22b06341edbb5f6f2f54e410383d5a
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-07-03 02:36:42 -0700

    Add React frontend for Home, Register, and SignIn pages with working registration connected to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,20 @@
-function App() {}
+import Home from "./pages/Home.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Register from "./pages/Register.jsx";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+"use client";
+import Navbar from "../components/Navbar";
+import Hero from "./Hero";
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
