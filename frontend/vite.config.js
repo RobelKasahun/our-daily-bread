8921f44cf98ed81198e6d20033163847955afa52
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-08-10 22:32:37 -0700

    Remove fontawesome that causing frontend errors

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,21 +5,21 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@fortawesome/fontawesome-svg-core': path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-svg-core'),
-    },
-  },
-  optimizeDeps: {
-    include: [
-      "@fortawesome/fontawesome-svg-core",
-      "@fortawesome/free-solid-svg-icons",
-      "@fortawesome/react-fontawesome",
-    ],
-  },
-  build: {
-    rollupOptions: {
-      external: ["@fortawesome/fontawesome-svg-core"],
-    },
-  }
+  // resolve: {
+  //   alias: {
+  //     '@fortawesome/fontawesome-svg-core': path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-svg-core'),
+  //   },
+  // },
+  // optimizeDeps: {
+  //   include: [
+  //     "@fortawesome/fontawesome-svg-core",
+  //     "@fortawesome/free-solid-svg-icons",
+  //     "@fortawesome/react-fontawesome",
+  //   ],
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["@fortawesome/fontawesome-svg-core"],
+  //   },
+  // }
 });
