4c2879d0727bb6d76fce8c22d32b96556ec0a5d0
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-08-10 21:35:15 -0700

    fix: vite config for fontawesome build issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  optimizeDeps: {
+    include: [
+      "@fortawesome/fontawesome-svg-core",
+      "@fortawesome/free-solid-svg-icons",
+      "@fortawesome/react-fontawesome",
+    ],
+  },
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/fontawesome-svg-core"],
+    },
+  },
 })
