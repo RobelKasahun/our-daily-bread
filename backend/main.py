995142f6c162a8d2fa6b4786b29730e3c744e390
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-08-10 21:08:14 -0700

    Remove debug mode

--- backend/main.py
+++ backend/main.py
@@ -5,4 +5,4 @@ app = create_app()
 # run the app in debug mode
 # run the app when the file is executed directly
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000)
