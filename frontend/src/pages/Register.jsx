19f7c8af725ccc53d50340e644f836418e4f9f83
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-08-14 16:50:28 -0700

    Fix broken logo urls part 3

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -48,8 +48,8 @@ export default function Register() {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 items-center overflow-hidden">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            alt="Your Company"
-            src="images/holyshare-logo.png"
+            alt="DailyBread"
+            src="images/dailybread-logo.png"
             className="mx-auto h-10 w-auto"
           />
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
