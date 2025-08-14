f443d9387ff28cb6d034ea1f2e79f6d02dd16e34
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-08-14 15:45:54 -0700

    Modify footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="footer border-t border-gray-200 p-3 text-center">
-      <p>© 2025 HolyShare. All rights reserved.</p>
+      <p>© 2025 DailyBread. All rights reserved.</p>
     </div>
   );
 }
