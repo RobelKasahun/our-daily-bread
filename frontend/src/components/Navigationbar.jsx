e25a6d5645d56917a9e33b09993b35ad7afafcca
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-08-16 19:32:58 -0700

    Add search bar for small devices

--- frontend/src/components/Navigationbar.jsx
+++ frontend/src/components/Navigationbar.jsx
@@ -324,22 +324,18 @@ export default function Navigationbar({
 
       <DisclosurePanel className="sm:hidden">
         <div className="space-y-1 px-2 pt-2 pb-3">
-          {/* {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? "page" : undefined}
-              className={classNames(
-                item.current
-                  ? "bg-gray-950/50 text-white"
-                  : "text-gray-300 hover:bg-white/5 hover:text-white",
-                "block rounded-md px-3 py-2 text-base font-medium"
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))} */}
+          {/* Search Bar */}
+          {showSearchBar && (
+            <form className="mx-8">
+              <input
+                type="text"
+                placeholder="Search"
+                // value={input}
+                // onChange={handleSearchInputChange}
+                className="w-full border border-gray-300 rounded-md px-4 py-2 text-base text-gray-900 placeholder-gray-400 focus:outline-none"
+              />
+            </form>
+          )}
         </div>
       </DisclosurePanel>
     </Disclosure>
