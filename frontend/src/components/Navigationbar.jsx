6dc81e60a5b497e09d9abbcbc187214cfbca93bc
RobelKasahun <36493926+RobelKasahun@users.noreply.github.com>
2025-08-16 19:54:11 -0700

    Add useEffects and fix currentUser undefined error

--- frontend/src/components/Navigationbar.jsx
+++ frontend/src/components/Navigationbar.jsx
@@ -198,17 +198,6 @@ import {
 //   );
 // }
 
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-} from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
